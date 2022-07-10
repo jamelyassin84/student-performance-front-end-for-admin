ae63e3611b584f07d2fbcb6eb59ae5aadc432cf4
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-10 22:00:31 +0400

    Student Details

--- src/app/modules/admin/students/student-details/student-details.component.ts
+++ src/app/modules/admin/students/student-details/student-details.component.ts
@@ -11,4 +11,93 @@ export class StudentDetailsComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {}
+
+    chart = {
+        chart: {
+            fontFamily: 'inherit',
+            foreColor: 'inherit',
+            height: '100%',
+            type: 'line',
+            toolbar: {
+                show: false,
+            },
+            zoom: {
+                enabled: false,
+            },
+        },
+        colors: ['#14B8A6', '#14B8A6'],
+        dataLabels: {
+            enabled: true,
+            enabledOnSeries: [0],
+            background: {
+                borderWidth: 0,
+            },
+        },
+        grid: {
+            borderColor: 'var(--fuse-border)',
+        },
+        labels: ['1st Yr-2nd Sem', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        legend: {
+            show: false,
+        },
+        plotOptions: {
+            bar: {
+                columnWidth: '50%',
+            },
+        },
+        series: [
+            {
+                name: '1',
+                data: [
+                    { x: '1st Yr - 1st Sem', y: [95] },
+                    { x: '1st Yr - 2nd Sem', y: [80] },
+                    { x: '2nd Yr - 1st Sem', y: [42] },
+                    { x: '2nd Yr - 2nd Sem', y: [31] },
+                    { x: '3rd Yr - 1st Sem', y: [45] },
+                    { x: '3rd Yr - 2nd Sem', y: [56] },
+                    { x: '4th Yr - 1st Sem', y: [78] },
+                    { x: '4th Yr - 2nd Sem', y: [78] },
+                ],
+            },
+        ],
+        states: {
+            hover: {
+                filter: {
+                    type: 'darken',
+                    value: 0.75,
+                },
+            },
+        },
+        stroke: {
+            width: [3, 0],
+        },
+        tooltip: {
+            followCursor: true,
+            theme: 'dark',
+        },
+        xaxis: {
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                color: 'var(--fuse-border)',
+            },
+            labels: {
+                style: {
+                    colors: 'var(--fuse-text-secondary)',
+                },
+            },
+            tooltip: {
+                enabled: false,
+            },
+        },
+        yaxis: {
+            labels: {
+                offsetX: -16,
+                style: {
+                    colors: 'var(--fuse-text-secondary)',
+                },
+            },
+        },
+    };
 }
--- src/app/modules/admin/students/students.component.ts
+++ src/app/modules/admin/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
 import { dbwAnimations } from '@global_packages/animations/animation.api';
 
 @Component({
@@ -8,7 +9,25 @@ import { dbwAnimations } from '@global_packages/animations/animation.api';
     animations: [...dbwAnimations],
 })
 export class StudentsComponent implements OnInit {
-    constructor() {}
+    constructor(private _confirm: FuseConfirmationService) {}
 
     ngOnInit(): void {}
+
+    remove(id: string) {
+        this._confirm.open({
+            title: `Are you sure you want to remove ernest sandoval?`,
+            message: `This will permanently remove his data. Continue?`,
+            dismissible: true,
+            icon: {
+                name: 'feather:trash',
+                color: 'accent',
+            },
+            actions: {
+                confirm: {
+                    color: 'accent',
+                    label: `Continue Remove`,
+                },
+            },
+        });
+    }
 }
