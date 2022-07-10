38cb5daf98eec9390f8fb7b155ef253f5224b7ee
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-10 22:10:46 +0400

    Survey

--- src/app/modules/admin/survey/survey.component.ts
+++ src/app/modules/admin/survey/survey.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
 import { dbwAnimations } from '@global_packages/animations/animation.api';
 
 @Component({
@@ -8,7 +9,25 @@ import { dbwAnimations } from '@global_packages/animations/animation.api';
     animations: [...dbwAnimations],
 })
 export class SurveyComponent implements OnInit {
-    constructor() {}
+    constructor(private _confirm: FuseConfirmationService) {}
 
     ngOnInit(): void {}
+
+    remove(id: string) {
+        this._confirm.open({
+            title: `Are you sure you want to remove this question?`,
+            message: `This will permanently remove this data. Continue?`,
+            dismissible: true,
+            icon: {
+                name: 'feather:trash',
+                color: 'accent',
+            },
+            actions: {
+                confirm: {
+                    color: 'accent',
+                    label: `Continue Remove`,
+                },
+            },
+        });
+    }
 }
