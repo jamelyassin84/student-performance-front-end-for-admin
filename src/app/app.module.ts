f500cf4a514ba1d2c4010d9e32bf33f1367a71c3
Jamel Yassin <jamelyassin84@gmail.com>
2022-09-22 20:22:36 +0400

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
 const routerConfig: ExtraOptions = {
     preloadingStrategy: PreloadAllModules,
     scrollPositionRestoration: 'enabled',
+    useHash: true,
 };
 
 @NgModule({
